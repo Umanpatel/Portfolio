52610cd611e3b0f7c614e3a866364e2fd62992f0
Umang Maheshkumar Patel <umpatel19041999@gmail.com>
2024-08-08 15:41:02 -0400

    BentoGrid Deleted

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,22 +22,6 @@ import ShineBorder from "@/components/magicui/shine-border";
 
 const BLUR_FADE_DELAY = 0.04;
 
-export function BentoGridDemo() {
-  return (
-    <BentoGrid className="max-w-4xl mx-auto">
-      {items.map((item, i) => (
-        <BentoGridItem
-          key={i}
-          title={item.title}
-          description={item.description}
-          header={item.header}
-          icon={item.icon}
-          className={i === 3 || i === 6 ? "md:col-span-2" : ""}
-        />
-      ))}
-    </BentoGrid>
-  );
-}
 const Skeleton = () => (
   <div className="flex flex-1 w-full h-full min-h-[6rem] rounded-xl bg-gradient-to-br from-neutral-200 dark:from-neutral-900 dark:to-neutral-800 to-neutral-100"></div>
 );
