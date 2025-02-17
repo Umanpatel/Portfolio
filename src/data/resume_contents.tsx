b7252686c66f87e1d33c5e2acedbebbdeaadc8be
Umang Maheshkumar Patel <umpatel19041999@gmail.com>
2025-02-17 16:18:57 -0500

    Change Resume Content

--- src/data/resume_contents.tsx
+++ src/data/resume_contents.tsx
@@ -182,7 +182,7 @@ export const DATA = {
     {
       title: "E-Commerce Microservices",
       href: "https://github.com/Umanpatel/E-Commerce_MicroServices",
-      dates: "August 2024 - Present",
+      dates: "August 2024",
       active: true,
       description:
         "This project is a basic e-commerce application designed using a microservices architecture. It includes distinct services for handling Products, Orders, Admin operations, and Image management, all coordinated through an API Gateway. Each service is ensuring scalability and flexibility in managing the application.",
