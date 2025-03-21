5d69c296cef07187ba31794ad153ab652c5ab033
Umang Maheshkumar Patel <umpatel19041999@gmail.com>
2025-03-21 03:38:17 -0400

    Slug Page for blog is coming

--- src/app/blogs/[slug]/page.tsx
+++ src/app/blogs/[slug]/page.tsx
@@ -52,7 +52,8 @@ export default async function BlogPost({ params }: BlogParams) {
 
   return (
     <main className="flex flex-col min-h-screen w-full lg:w-[55%] m-auto p-7 border-2 lg:p-10">
-      <article className="prose dark:prose-invert max-w-none">
+      Exciting full blog page is coming soon, Stay Tune and Keep Coding.
+      {/* <article className="prose dark:prose-invert max-w-none">
         <BlurFade delay={BLUR_FADE_DELAY * 11}>
           <header className="mb-8">
             <h1 className="text-3xl font-bold tracking-tight md:text-4xl mb-2">
@@ -94,7 +95,7 @@ export default async function BlogPost({ params }: BlogParams) {
             <ShareButtons url={currentUrl} title={post.title} />
           </div>
         </BlurFade>
-      </article>
+      </article> */}
     </main>
   );
 }
